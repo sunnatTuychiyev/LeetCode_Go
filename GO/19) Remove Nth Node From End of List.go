cf65b3aea113d1638401d1911bf57ed754540f3e
Tuychiyev Sunnatillo <151430152+sunnatTuychiyev@users.noreply.github.com>
2024-04-19 20:21:41 +0500

    19. Remove Nth Node From End of List(#19)

---
+++ GO/19) Remove Nth Node From End of List.go
@@ -0,0 +1,17 @@
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	fast, slow := dummy, dummy
+
+	for i := 0; i <= n; i++ {
+		fast = fast.Next
+	}
+
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+
+	slow.Next = slow.Next.Next
+
+	return dummy.Next
+}
