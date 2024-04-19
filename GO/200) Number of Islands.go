c4cac5c9dfc0171428eb4d0d84179e784d893cc1
Tuychiyev Sunnatillo <151430152+sunnatTuychiyev@users.noreply.github.com>
2024-04-19 15:28:34 +0500

    200. Number of Islands (200)

---
+++ GO/200) Number of Islands.go
@@ -0,0 +1,23 @@
+func numIslands(grid [][]byte) int {
+	count := 0
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] == '1' {
+				count++
+				dfs(grid, i, j)
+			}
+		}
+	}
+	return count
+}
+
+func dfs(grid [][]byte, i, j int) {
+	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[i]) || grid[i][j] == '0' {
+		return
+	}
+	grid[i][j] = '0'
+	dfs(grid, i+1, j)
+	dfs(grid, i-1, j)
+	dfs(grid, i, j+1)
+	dfs(grid, i, j-1)
+}
